cf2c0e1fdfe58a1e4da00c54b78a3fabbbd1dca6
Sanjay Singh <itssanjay@gmail.com>
2017-07-01 15:30:36 -0400

    Revert "Fixing issue with reading of appsettings"
    This reverts commit 9c394d07a36f5273bb1d8668d938d116bb8039db.

--- src/MabWeb/Startup.cs
+++ src/MabWeb/Startup.cs
@@ -15,7 +15,7 @@ namespace MabWeb
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
@@ -26,8 +26,11 @@ namespace MabWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOptions();
-            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            var appSettings = Configuration.GetSection("AppSettings");
+
+            services.Configure<AppSettings>(appSettings);
+
+            // Add framework services.
             services.AddMvc();
         }
 
